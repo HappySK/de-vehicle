1f435f49bb68e38e7c1c2c2f2b0c0bf364a78839
Sravan Kumar <shravanyoserene1729@gmail.com>
2024-03-09 22:13:11 +0530

    Modify source file path

--- load_vehicle_data.py
+++ load_vehicle_data.py
@@ -1,5 +1,5 @@
 from pyspark.sql import SparkSession
 
 spark = SparkSession.builder.appName('vehicle_makes').getOrCreate()
-makes = spark.read.option('header','true').csv('vehicle_makes.csv')
+makes = spark.read.option('header','true').csv('/home/airflow_user/vehicle_makes.csv')
 makes.show(truncate=False)
