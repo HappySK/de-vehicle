3e81768e9311d701da00545ecb8a16bf6ae945e8
Sravan Kumar <shravanyoserene1729@gmail.com>
2024-03-10 15:04:00 +0530

    Add HDFS path

--- get_vehicle_data.py
+++ get_vehicle_data.py
@@ -2,6 +2,6 @@ import requests
 
 base_url = 'https://vpic.nhtsa.dot.gov/api'
 res = requests.get(f'{base_url}/vehicles/GetAllMakes?format=csv')
-with open('vehicle_makes.csv', 'w') as f:
+with open('hdfs://spark-driver.desk.home:9000/vehicle_makes.csv', 'w') as f:
     f.write(res.text)
 print('Write Completed')
--- load_vehicle_data.py
+++ load_vehicle_data.py
@@ -1,5 +1,5 @@
 from pyspark.sql import SparkSession
 
 spark = SparkSession.builder.appName('vehicle_makes').getOrCreate()
-makes = spark.read.option('header','true').csv('/home/airflow_user/vehicle_makes.csv')
+makes = spark.read.option('header','true').csv('hdfs://spark-driver.desk.home:9000/vehicle_makes.csv')
 makes.show(truncate=False)
