66b6784ebd0a1644e39440552e4677eddf5cbd94
Sravan Kumar <shravanyoserene1729@gmail.com>
2024-03-10 19:04:14 +0530

    Add HDFS Path to vehicle data load

--- load_vehicle_data.py
+++ load_vehicle_data.py
@@ -1,5 +1,5 @@
 from pyspark.sql import SparkSession
 
 spark = SparkSession.builder.appName('vehicle_makes').getOrCreate()
-makes = spark.read.option('header','true').csv('vehicle_makes.csv')
+makes = spark.read.option('header','true').csv('hdfs://spark-driver.desk.home:9000/user/spark_driver_user/vehicle_makes.csv')
 makes.show(truncate=False)
