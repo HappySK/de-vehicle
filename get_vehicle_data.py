7b5a42088232920d673285afab4658981c227dc0
Sravan Kumar <shravanyoserene1729@gmail.com>
2024-03-10 15:52:40 +0530

    Add the csv file into HDFS

--- get_vehicle_data.py
+++ get_vehicle_data.py
@@ -1,7 +1,13 @@
-import requests
+import requests, os
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.appName('get_vehicle_data').getOrCreate()
 
 base_url = 'https://vpic.nhtsa.dot.gov/api'
 res = requests.get(f'{base_url}/vehicles/GetAllMakes?format=csv')
-with open('hdfs://spark-driver.desk.home:9000/vehicle_makes.csv', 'w') as f:
+with open('vehicle_makes.csv', 'w') as f:
     f.write(res.text)
 print('Write Completed')
+
+makes_df = spark.read.option('header','true').csv('/home/airflow_user/vehicle_makes.csv')
+makes_df.write.csv('hdfs://spark-driver.desk.home:9000/vehicle_csv')
