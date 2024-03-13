8bd2fffc0407b51098b0935d926fcf9836e7dc71
Sravan Kumar <shravanyoserene1729@gmail.com>
2024-03-13 23:25:49 +0530

    Save table from DF

--- load_vehicle_data.py
+++ load_vehicle_data.py
@@ -2,4 +2,4 @@ from pyspark.sql import SparkSession
 
 spark = SparkSession.builder.appName('vehicle_makes').getOrCreate()
 makes = spark.read.option('header','true').csv('hdfs://spark-driver.desk.home:9000/user/spark_driver_user/vehicle_makes.csv')
-makes.show(truncate=False)
+makes.saveAsTable('makes')
