bc38ad7048cdec2599e873833c7255e47e2f0d1b
Sravan Kumar <shravanyoserene1729@gmail.com>
2024-03-14 23:34:19 +0530

    Add warehouse dir

--- load_vehicle_data.py
+++ load_vehicle_data.py
@@ -1,5 +1,5 @@
 from pyspark.sql import SparkSession
 
-spark = SparkSession.builder.appName('vehicle_makes').getOrCreate()
+spark = SparkSession.builder.appName('vehicle_makes').config('spark.sql.warehouse.dir','webhdfs://spark-driver.desk.home:9000/user/spark_driver_user/vehicle/').getOrCreate()
 makes = spark.read.option('header','true').csv('hdfs://spark-driver.desk.home:9000/user/spark_driver_user/vehicle_makes.csv')
-makes.saveAsTable('makes')
+makes.write.saveAsTable('makes')
