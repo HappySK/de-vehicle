beb3df2037b91f6ac140be0ef3ce412ee5295e2e
Sravan Kumar <shravanyoserene1729@gmail.com>
2024-03-16 20:25:22 +0530

    Add JDBC Logic

--- load_vehicle_data.py
+++ load_vehicle_data.py
@@ -1,13 +1,8 @@
 from pyspark.sql import SparkSession
 from airflow.models.connection import Connection
 
-conn = Connection(
-    conn_id="jdbc-conn-id",
-    description="Establish connection between Spark & MySQL"
-)
-
 spark = SparkSession.builder.appName('vehicle_makes').getOrCreate()
 makes = spark.read.option('header', 'true').csv(
     'hdfs://spark-driver.desk.home:9000/user/spark_driver_user/vehicle_makes.csv')
 
-print(conn)
+makes.write.jdbc('jdbc://mysql.desk.home:3306/vehicle','makes', mode='overwrite',properties={"user": "spark_user", "password": "msk","driver": "com.mysql.cj.jdbc.Driver"})
