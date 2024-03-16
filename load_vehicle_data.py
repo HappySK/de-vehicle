0bd562d6649916da79434685c29031ea2f2f9840
Sravan Kumar <shravanyoserene1729@gmail.com>
2024-03-16 15:08:06 +0530

    Print conn

--- load_vehicle_data.py
+++ load_vehicle_data.py
@@ -1,5 +1,13 @@
 from pyspark.sql import SparkSession
+from airflow.models.connection import Connection
 
-spark = SparkSession.builder.appName('vehicle_makes').config('spark.sql.warehouse.dir','webhdfs://spark-driver.desk.home:9000/user/spark_driver_user/vehicle/').getOrCreate()
-makes = spark.read.option('header','true').csv('hdfs://spark-driver.desk.home:9000/user/spark_driver_user/vehicle_makes.csv')
-makes.write.saveAsTable('makes')
+conn = Connection(
+    conn_id="jdbc-conn-id",
+    description="Establish connection between Spark & MySQL"
+)
+
+spark = SparkSession.builder.appName('vehicle_makes').getOrCreate()
+makes = spark.read.option('header', 'true').csv(
+    'hdfs://spark-driver.desk.home:9000/user/spark_driver_user/vehicle_makes.csv')
+
+print(conn)
