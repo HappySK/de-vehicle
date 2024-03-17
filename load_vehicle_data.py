663de2d12b5b70e4a537fd28ebd1c7e9097ca21a
Sravan Kumar <shravanyoserene1729@gmail.com>
2024-03-17 19:11:09 +0530

    Add params for JDBC URL

--- load_vehicle_data.py
+++ load_vehicle_data.py
@@ -4,4 +4,4 @@ spark = SparkSession.builder.appName('vehicle_makes').getOrCreate()
 makes = spark.read.option('header', 'true').csv(
     'hdfs://spark-driver.desk.home:9000/user/spark_driver_user/vehicle_makes.csv')
 
-makes.write.jdbc('jdbc:mysql://mysql.desk.home:3306/vehicle','makes', mode='overwrite',properties={"user": "spark_user", "password": "msk","driver": "com.mysql.cj.jdbc.Driver"})
+makes.write.jdbc('jdbc:mysql://mysql.desk.home:3306/vehicle?allowPublicKeyRetrieval=true&useSSL=false','makes', mode='overwrite',properties={"user": "spark_user", "password": "msk","driver": "com.mysql.cj.jdbc.Driver"})
