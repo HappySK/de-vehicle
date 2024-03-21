485b9827e0b2bdac8fa21f9af70bb00279feb49d
Sravan Kumar <shravanyoserene1729@gmail.com>
2024-03-21 22:14:42 +0530

    Modify password retrival logic

--- load_vehicle_data.py
+++ load_vehicle_data.py
@@ -1,6 +1,9 @@
 import sys
 
 from pyspark.sql import SparkSession
+from airflow.models import Variable
+
+mysql_pwd = Variable.get("foo")
 
 api_action = sys.argv[1]
 table_name = api_action.replace('Get', '')
@@ -13,6 +16,6 @@ print(f'Data Frame read is completed for {api_action}')
 
 makes.write.jdbc('jdbc:mysql://mysql.desk.home:3306/vehicle?allowPublicKeyRetrieval=true&useSSL=false', table_name,
                  mode='overwrite',
-                 properties={"user": "spark_user", "password": "msk", "driver": "com.mysql.cj.jdbc.Driver"})
+                 properties={"user": "spark_user", "password": f"{mysql_pwd}", "driver": "com.mysql.cj.jdbc.Driver"})
 
 print(f'The Data is loaded into MySQL for {table_name}')
